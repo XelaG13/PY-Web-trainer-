df41b4a1ceed99a74106d7ad70c03f2336bc0e24
Ashraf Salhi <161850173+XelaG13@users.noreply.github.com>
2024-04-05 02:39:59 +0100

    PY trainer web

---
+++ README.md
@@ -0,0 +1,1 @@
+"# PY-Web-trainer-" 
---
+++ ash.py
@@ -0,0 +1,42 @@
+import os
+
+
+
+ashraf = ("esmi zelag")
+
+print (ashraf)
+
+a = 5
+b = 5
+c = 2 
+
+print (b+a+c)
+
+c = input ("dekhel mot de passe ")
+
+
+
+# conditions ash ashraf xelag
+
+if (c) == ("ash"):
+    print ("good password")
+
+elif (c) == ("ashraf"):
+    print ("good password")
+
+elif (c) == ("xelag"):
+    print ("good password")
+
+else :
+    print ("wrong password")
+    
+    
+
+
+
+ 
+
+
+
+
+
